033916b947d4c256ce343ffeb1f05f3f3f23a777
PoonPooncharat <s6301012620146@email.kmutnb.ac.th>
2022-08-06 12:59:08 +0700

    Part 1 finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,20 +4,38 @@
 
 def sum arr
   # YOUR CODE HERE
+  result = 0
+  arr.each {|num| result += num}
+  result
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  # YOUR CODE
+  return 0 if arr.length == 0
+  return arr[0] if arr.length == 1
+  arr.sort! {|firstnum, secondnum| secondnum <=> firstnum}
+  return arr[0] + arr[1]
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  return false if arr.length <= 1
+  arr.each {|firstnum| arr.each {|secondnum|
+    if firstnum == secondnum
+      next
+    end
+    if firstnum + secondnum == n
+      return true
+    end
+    } }
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+
 end
 
 def starts_with_consonant? s
