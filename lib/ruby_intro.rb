dd9cc9739ce15dfdae48058c562c6ecd957e496b
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2022-08-06 14:02:19 +0700

    part 2 3 finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -35,19 +35,42 @@ end
 
 def hello(name)
   # YOUR CODE HERE
-
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  vowel = ["a","e","i","o","u"]
+  first_letter = s[0]
+  return false if first_letter == "" or first_letter.nil?
+  return false if vowel.include? first_letter.downcase
+  return false unless first_letter.match?(/[[:alpha:]]/)
+  return true
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  return false if s.match?(/[[:alpha:]]/) or s == ""
+  binary = s.to_i.to_s(2)
+  return true if binary.to_i % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(isbn,price)
+    if isbn == "" or price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
